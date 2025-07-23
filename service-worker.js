05cd20fe7ca305be658f518468d8614a82e397c3
Onigiri-hub <imomlk8@gmail.com>
2025-07-24 07:53:04 +0900

    PWA化

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,28 @@
 const CACHE_NAME = "stampcard-cache-v1";
 const urlsToCache = [
+  "./",
   "./index.html",
   "./style.css",
   "./script.js",
   "./manifest.json",
   "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-512.png",
+  "./icons/img01.svg",
+  "./icons/img02.svg",
+  "./sounds/001.mp3",
+  "./sounds/002.mp3"
 ];
 
 // インストール時にキャッシュ
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// オフライン対応: キャッシュ優先で取得
+// オフライン対応（キャッシュ優先）
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
